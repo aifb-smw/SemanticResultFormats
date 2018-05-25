1028cef9f9c462b5e485a69b1e0e7b846be70fbc
JochenU <33481256+JochenU@users.noreply.github.com>
2018-05-25 13:27:30 +0200

    test

--- formats/bibtex/SRF_BibTeX.php
+++ formats/bibtex/SRF_BibTeX.php
@@ -453,7 +453,7 @@ class SMWBibTeXEntry {
 
 		$latex_equivalents = array (
 		    // Zus�tzliche Zeichen -- mm
-		    ord('$') => '\$',
+		    ord('$') => '\$test',
 		    ord('_') => '\_',
 		    ord('{') => '\{',
 		    ord('}') => '\}',
